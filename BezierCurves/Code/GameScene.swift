8a31d5512d962bba3db0b64662609b196b07ab45
Jeremy Conkin <jconkin@gmail.com>
2022-12-03 21:19:32 -0700

    Draw and nimate secondary lines

--- BezierCurves/Code/GameScene.swift
+++ BezierCurves/Code/GameScene.swift
@@ -2,6 +2,12 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var startControlSegmentCircleNode: SKShapeNode?
+    var centerControlSegmentCircleNode: SKShapeNode?
+    var endControlSegmentCircleNode: SKShapeNode?
+    var startSubcontrolLineNode: SKShapeNode?
+    var endSubcontrolLineNode: SKShapeNode?
+
     var lineSegment: LineSegment? {
         didSet {
             redrawScene()
@@ -22,7 +28,7 @@ class GameScene: SKScene {
         drawLineSegment(segment: lineSegment,
                         lineColor: .white,
                         dotFillColor: .blue,
-                        dotPathColor: .blue,
+                        dotStrokeColor: .blue,
                         isDashed: true)
 
         let controlPoint1 = CGPoint(x: -250, y: 100)
@@ -31,7 +37,7 @@ class GameScene: SKScene {
         drawLineSegment(segment: controlPoint1Segment,
                         lineColor: .blue,
                         dotFillColor: .clear,
-                        dotPathColor: .blue)
+                        dotStrokeColor: .blue)
 
         let controlPoint2 = CGPoint(x: 120, y: 210)
         let controlPoint2Segment = LineSegment(startingPoint: lineSegment.endingPoint,
@@ -39,21 +45,69 @@ class GameScene: SKScene {
         drawLineSegment(segment: controlPoint2Segment,
                         lineColor: .blue,
                         dotFillColor: .clear,
-                        dotPathColor: .blue)
+                        dotStrokeColor: .blue)
 
         let controlSegment = LineSegment(startingPoint: controlPoint1,
                                          endingPoint: controlPoint2)
         drawLineSegment(segment: controlSegment,
                         lineColor: .white,
                         dotFillColor: .clear,
-                        dotPathColor: .blue,
+                        dotStrokeColor: .blue,
                         isDashed: true)
+
+        startControlSegmentCircleNode = interpolateLineSegment(segment: controlPoint1Segment,
+                                                                dotFillColor: .green,
+                                                                dotStrokeColor: .green)
+        centerControlSegmentCircleNode = interpolateLineSegment(segment: controlSegment,
+                                                                dotFillColor: .green,
+                                                                dotStrokeColor: .green)
+        endControlSegmentCircleNode = interpolateLineSegment(segment: controlPoint2Segment,
+                                                             dotFillColor: .green,
+                                                             dotStrokeColor: .green,
+                                                             reverse: true)
+        let path1 = CGMutablePath();
+        path1.move(to: controlPoint1Segment.startingPoint)
+        path1.addLine(to: controlSegment.startingPoint)
+        startSubcontrolLineNode = SKShapeNode(path: path1)
+        startSubcontrolLineNode?.strokeColor = .green
+        addChild(startSubcontrolLineNode!)
+
+        let path2 = CGMutablePath();
+        path2.move(to: controlPoint2Segment.endingPoint)
+        path2.addLine(to: controlSegment.startingPoint)
+        endSubcontrolLineNode = SKShapeNode(path: path2)
+        endSubcontrolLineNode?.strokeColor = .green
+        addChild(endSubcontrolLineNode!)
+    }
+
+    func updateStartSubcontrolLineNode() {
+        guard let startControlSegmentCircleNode = startControlSegmentCircleNode,
+            let centerControlSegmentCircleNode = centerControlSegmentCircleNode,
+            let startSubcontrolLineNode = startSubcontrolLineNode else {
+            return
+        }
+        let path = CGMutablePath();
+        path.move(to: startControlSegmentCircleNode.position)
+        path.addLine(to: centerControlSegmentCircleNode.position)
+        startSubcontrolLineNode.path = path
+    }
+
+    func updateEndSubcontrolLineNode() {
+        guard let endControlSegmentCircleNode = endControlSegmentCircleNode,
+            let centerControlSegmentCircleNode = centerControlSegmentCircleNode,
+            let endSubcontrolLineNode = endSubcontrolLineNode else {
+            return
+        }
+        let path = CGMutablePath();
+        path.move(to: centerControlSegmentCircleNode.position)
+        path.addLine(to: endControlSegmentCircleNode.position)
+        endSubcontrolLineNode.path = path
     }
 
     func drawLineSegment(segment: LineSegment,
                          lineColor: SKColor,
                          dotFillColor: SKColor,
-                         dotPathColor: SKColor,
+                         dotStrokeColor: SKColor,
                          isDashed:Bool = false) {
         let path = CGMutablePath();
         path.move(to: segment.startingPoint)
@@ -68,14 +122,40 @@ class GameScene: SKScene {
         let circleRadius: CGFloat = 5
         let startCircle = SKShapeNode(circleOfRadius: circleRadius)
         startCircle.position = segment.startingPoint
-        startCircle.strokeColor = dotPathColor
+        startCircle.strokeColor = dotStrokeColor
         startCircle.fillColor = dotFillColor
         addChild(startCircle)
 
         let endCircle = SKShapeNode(circleOfRadius: circleRadius)
         endCircle.position = segment.endingPoint
-        endCircle.strokeColor = dotPathColor
+        endCircle.strokeColor = dotStrokeColor
         endCircle.fillColor = dotFillColor
         addChild(endCircle)
+    }
+
+    @discardableResult
+    func interpolateLineSegment(segment: LineSegment,
+                                dotFillColor: SKColor,
+                                dotStrokeColor: SKColor,
+                                reverse: Bool = false) -> SKShapeNode {
+
+        let circleRadius: CGFloat = 5
+        let circle = SKShapeNode(circleOfRadius: circleRadius)
+        let start = reverse ? segment.endingPoint : segment.startingPoint
+        let end = reverse ? segment.startingPoint : segment.endingPoint
+        circle.position = start
+        circle.strokeColor = dotStrokeColor
+        circle.fillColor = dotFillColor
+        addChild(circle)
+
+        let moveAction = SKAction.move(to: end, duration: 2.0)
+        circle.run(moveAction)
+
+        return circle
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        updateStartSubcontrolLineNode()
+        updateEndSubcontrolLineNode()
     }
 }
